6f490fd6cddcc1c7a31b97ab56372687a9c50a1e
Maulizar1504 <167389695+Maulizar1504@users.noreply.github.com>
2024-11-27 09:20:21 +0700

    QRIS.java

--- QRISPayment.java
+++
@@ -1,6 +0,0 @@
-public class QRISPayment implements PaymentMethod {
-    @Override
-    public void pay(int amount) {
-        System.out.println("Pembayaran melalui QRIS sebesar Rp" + amount + " berhasil diproses.");
-    }
-}
