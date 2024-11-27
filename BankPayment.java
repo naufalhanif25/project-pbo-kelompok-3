0880dd6865779e69b84ac015211900fcecf98d57
Maulizar1504 <167389695+Maulizar1504@users.noreply.github.com>
2024-11-27 09:14:45 +0700

    Bank.java

--- BankPayment.java
+++
@@ -1,6 +0,0 @@
-public class BankPayment implements PaymentMethod {
-    @Override
-    public void pay(int amount) {
-        System.out.println("Pembayaran melalui Bank Transfer sebesar Rp" + amount + " berhasil diproses.");
-    }
-}
