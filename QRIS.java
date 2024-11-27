961778d162f8d26a70db925d1a82f444040db9ac
Naufal Hanif <166219072+naufalhanif25@users.noreply.github.com>
2024-11-27 18:23:48 +0700

    Delete QRIS.java

--- QRIS.java
+++
@@ -1,7 +0,0 @@
-public class QRIS implements PaymentMethod {
-    @Override
-    public void pay(int amount) {
-        System.out.println("Metode pembayaran: QRIS");
-        System.out.println("Silakan scan QRIS untuk membayar sebesar Rp" + amount);
-    }
-}
