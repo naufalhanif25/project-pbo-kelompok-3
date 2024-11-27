19c4f1eea6fa34d6f184c313c436f11538e0d2dd
Naufal Hanif <166219072+naufalhanif25@users.noreply.github.com>
2024-11-27 18:24:05 +0700

    Delete COD.java

--- COD.java
+++
@@ -1,7 +0,0 @@
-public class COD implements PaymentMethod {
-    @Override
-    public void pay(int amount) {
-        System.out.println("Metode pembayaran: COD (Cash on Delivery)");
-        System.out.println("Pesanan Anda akan dikirimkan, dan pembayaran sebesar Rp" + amount + " dilakukan saat barang diterima.");
-    }
-}
