7295c1d210c293827f783425fadd1eae2e319bb0
Naufal Hanif <166219072+naufalhanif25@users.noreply.github.com>
2024-11-27 18:24:19 +0700

    Delete Bank.java

--- Bank.java
+++
@@ -1,7 +0,0 @@
-public class Bank implements PaymentMethod {
-    @Override
-    public void pay(int amount) {
-        System.out.println("Metode pembayaran: Bank Transfer");
-        System.out.println("Pembayaran sebesar Rp" + amount + " harus ditransfer ke rekening kami.");
-    }
-}
